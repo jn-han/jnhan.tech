9f2b4253f68622c5e12092bc6cb9e7559a10f6f2
Joshua Nguyen <joshuahnguyen@gmail.com>
2022-05-12 00:36:59 -0700

    added animations, and introduction section

---
+++ src/components/Home.vue
@@ -0,0 +1,102 @@
+<template>
+
+    <NavBar />
+    <div class="main">
+        <div class="intro">
+            <h2 class="smallIntro">Hi, my name is</h2>
+            <h1 class="nameTitle">Joshua Nguyen</h1>
+            <p class="pIntro">An aspiring software engineer studying at the University of British Columbia</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+
+export default {
+    name: 'Home',
+    components: {
+    NavBar,
+}
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Mono&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+:root {
+    --bg-color: #1c2541;
+    --typewriterSpeed: 1.5s;
+    --typewriterCharacters: 14;
+}
+.main {
+    position: relative;
+    top: 150px;
+    justify-content: left;
+}
+.nameTitle {
+    font-family: Montserrat, sans-serif;
+    color: #f1e9e9;
+    opacity: 0;
+    transform: translateY(3rem);
+    animation: fadeInUp 2s ease 3.5s forwards;
+}
+
+.pIntro {
+    font-size: 20px;
+    font-family: Montserrat, sans-serif;
+    color: #99acab ;
+    opacity: 0;
+    transform: translateY(3rem);
+    animation: fadeInUp 2s ease 4.5s forwards;
+}
+
+.smallIntro {
+    color: #6FFFE9;
+    font-family: 'Roboto Mono', monospace;
+    font-size: clamp(1rem,3vw+1rem, 4rem);
+    position: relative;
+    width: max-content;
+}
+
+.smallIntro::before,
+.smallIntro::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+.smallIntro::before {
+    background: #1c2541;
+    animation: typewriter 1.5s steps(14) 1s forwards;
+}
+.smallIntro::after {
+    background: white;
+    width: 0.125em;
+    animation: 
+    typewriter 1.5s steps(14) 1s forwards, 
+    blink 1000ms steps(14) infinite;
+}
+
+@keyframes typewriter {
+    to {
+        left: 100%;
+    }
+}
+
+@keyframes blink {
+    to {
+        background: transparent;
+    }
+}
+@keyframes fadeInUp {
+    to {
+        opacity: 1;
+        transform: translateY(0)
+    }
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+<header>
+    <div>
+        <router-link class="logo" to="/">JN</router-link>
+    </div>
+    <nav class="nav_links">
+        <router-link to="/about"><span class="num">1. </span>About</router-link>
+        <router-link to="/projects"><span class="num">2. </span>Projects</router-link>
+        <router-link to="/contact"><span class="num">3. </span>Contact</router-link>
+        <router-link to="/resume" class="resume">Resume</router-link>
+    </nav>
+
+</header>
+
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+.logo {
+    font-size: 20px;
+    text-decoration: none;
+    color: inherit;
+}
+nav a {
+    text-decoration: none;
+    color: inherit;
+    padding: 20px;
+    position: relative;
+    
+}
+.resume {
+    color: #6FFFE9;
+    border:solid #6FFFE9;
+    border-radius: 15px;
+    padding: 15px;
+    right: -10px;
+}
+nav a:hover {
+    color: #6FFFE9;
+    transition: color 0.5s ease-in-out;
+}
+
+.num {
+    color: #6FFFE9;
+}
+
+header {
+    font-family: 'Montserrat', sans-serif;
+    color: #95a9a6;
+    position: fixed;
+    font-size: 17px;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    z-index: 99;
+    top: 5px;
+    right: 0%;
+    left: 0%;
+    right: 0%;
+}
+</style>
